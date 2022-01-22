89e7b0fe71e8057af5467ffb78325db329dbdc2b
Sébastien Vallet <sebastien.vallet89@gmail.com>
2022-01-22 17:30:29 +0100

    WIP: homepage

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,13 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+export interface ResponseDialog {
+  successUpload: SuccessUploadResponse | null,
+  errorUpload: HttpErrorResponse | null
+}
+export interface SuccessUploadResponse {
+  link: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +16,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'clipystream-front';
+
+  constructor() {
+    // TODO remove me
+    // this.uploadResponseDialog.success = {
+    //   link: "https://google.com"
+    // }
+
+    // this.uploadResponseDialog.error = {
+    //   error: "Bad Request"
+    // }
+  }
+
+  uploadResponseDialog: ResponseDialog = {
+    successUpload: null,
+    errorUpload: null
+  }
+
+  showUploadSuccess(successResponse: SuccessUploadResponse): void {
+    // TODO
+    console.log(successResponse);
+    this.setSuccessUpload(successResponse);
+
+  }
+
+  showUploadError(errorResponse: HttpErrorResponse): void {
+    // TODO
+    console.log(errorResponse);
+    this.setErrorUpload(errorResponse);
+  }
+
+  onCopiedLinkToClipboard(e: Event) {
+    // TODO
+    console.log(e);
+  }
+
+  private setSuccessUpload(data: SuccessUploadResponse) {
+    this.uploadResponseDialog.successUpload = data;
+    this.uploadResponseDialog.errorUpload = null;
+  }
+
+  private setErrorUpload(data: HttpErrorResponse) {
+    this.uploadResponseDialog.successUpload = null;
+    this.uploadResponseDialog.errorUpload = data;
+  }
+
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,36 @@
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DropzoneComponent } from './components/dropzone/dropzone.component';
+import { DragDropDirective } from './directives/drag-drop.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropzoneComponent,
+    DragDropDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ClipboardModule,
+    FormsModule,
+
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
